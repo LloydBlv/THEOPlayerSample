1116436beccc8f96de6695949e58faba07e1eddd
MohammadReza Najafi <mrnj92@gmail.com>
2018-11-08 20:00:39 +0330

    added asset sample parsing exception handler, removed redundant styles

--- app/src/main/kotlin/ir/zinutech/android/theoplayersample/features/SampleListActivity.kt
+++ app/src/main/kotlin/ir/zinutech/android/theoplayersample/features/SampleListActivity.kt
@@ -1,11 +1,13 @@
 package ir.zinutech.android.theoplayersample.features
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ir.zinutech.android.theoplayersample.R.layout
 import ir.zinutech.android.theoplayersample.core.utils.Constants
 import ir.zinutech.android.theoplayersample.core.utils.XmlParser
 import kotlinx.android.synthetic.main.activity_main.main_activity_view_pager
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -13,11 +15,16 @@ import kotlinx.coroutines.launch
 
 class SampleListActivity : AppCompatActivity() {
 
+  private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+    Toast.makeText(this@SampleListActivity, throwable.message, Toast.LENGTH_LONG).show()
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(layout.activity_main)
 
-    GlobalScope.launch(Dispatchers.Main) {
+    GlobalScope.launch(Dispatchers.Main + exceptionHandler) {
+
 
       val samplesInputStream = assets.open(Constants.SAMPLES_FILE_NAME)
       val samples = async(Dispatchers.IO) {
@@ -25,6 +32,7 @@ class SampleListActivity : AppCompatActivity() {
       }.await()
 
       main_activity_view_pager.adapter = SamplesPagerAdapter(samples)
+
     }
 
   }
